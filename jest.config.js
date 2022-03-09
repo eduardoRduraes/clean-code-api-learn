1d26fc01a688acea74061cd804a81c61b3d584a8
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-03-08 21:31:20 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -22,6 +22,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: "@shelf/jest-mongodb",
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
--- src/infra/criptografia/bcrypt-adapter.spec.ts
+++ src/infra/criptografia/bcrypt-adapter.spec.ts
@@ -39,7 +39,7 @@ describe('Bcrypt Adapter', () =>{
     const { sut } = makeSut()
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(
       new Promise((resolve,reject)=> reject(new Error())
-      ))
+    ))
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
   })
