4803f25a50b92f2bdbdde4d55d2a1bc06e14d833
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-03-09 22:33:53 -0300

    feat: ensure requeest return json as default content type

--- src/main/config/middewares.ts
+++ src/main/config/middewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middewares/body-parser'
 import { cors } from '../middewares/cors'
+import { contentType } from '../middewares/content-type'
 
 export default (app: Express):void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
