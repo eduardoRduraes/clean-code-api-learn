7a41d115fcb60dedb7566a1cb337cf3a99244372
Eduardo R Duraes <eduardoduraes.bsi@gmail.com>
2022-03-15 15:24:21 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,7 +2,7 @@ import { Controller, HttpRequest, HttpResponse } from "../../presentation/protoc
 import { LogControllerDecorator } from "./log"
 
 interface SutTypes {
-  sut:LogControllerDecorator,
+  sut: LogControllerDecorator,
   controllerStub: Controller
 }
 
@@ -44,5 +44,24 @@ describe('LogController Decorator', () =>{
     }
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async() => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body:{
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode : 200,
+      body:{
+        name: 'Rodrigo'
+      }
+    })
   })
 })
