aa3572d94890999871011b42a923579a4e7c6d45
Eduardo R Duraes <eduardoduraes.bsi@gmail.com>
2022-03-16 16:42:41 -0300

    refactor: reload files by error using wsl vs-code

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,6 +2,7 @@ import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DBAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
@@ -13,7 +14,7 @@ export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const dbAddAccount = new DBAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  const logMongoRepository: any = null
+  const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
 
