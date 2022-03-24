3735ed8a0f42c8b0557b92bfd4e0fce55bd20fb6
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-03-24 13:53:57 -0300

    feat: ensure BcryptAdapter returns true when compare success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -54,4 +54,10 @@ describe('Bcrypt Adapter', () =>{
     await sut.compare('any_value','any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value','any_hash')
   })
+
+   test('Should return true when compare success', async () =>{
+    const { sut } = makeSut()
+    const isvalid = await sut.compare('any_value','any_hash')
+    expect(isvalid).toBe(true)
+  })
 })
