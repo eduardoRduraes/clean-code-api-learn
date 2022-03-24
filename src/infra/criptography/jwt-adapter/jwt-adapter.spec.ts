7b29690b006e6b262762631f692680f60f5ffd87
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-03-24 14:48:17 -0300

    test: ensure JwtAdapter throw if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -31,4 +31,11 @@ describe('Jwt Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const {sut} = makeSut('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(()=>{ throw new Error()})
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
