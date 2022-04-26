a077ea0e96413a9799223ce31e7f592ac6f4cd97
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-04-26 05:45:36 -0300

    feat: ensure LoadSurveysController returns 204 if there is no content

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurvey, ok, serverError } from "./load-survey-controller-protocols";
+import { Controller, HttpRequest, HttpResponse, LoadSurvey, noContent, ok, serverError } from "./load-survey-controller-protocols";
 
 import {} from '../../../../domain/usecases/load-survey'
 export class LoadSurveyController implements Controller {
@@ -8,7 +8,7 @@ export class LoadSurveyController implements Controller {
   async handle(httpRequest: HttpRequest):Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurvey.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }
