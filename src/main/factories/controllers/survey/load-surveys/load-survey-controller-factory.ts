6356738f5a8e50d0f271c449ab935eb2c7d6d0f3
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-04-26 15:41:20 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-survey-controller-factory.ts
@@ -0,0 +1,10 @@
+import { makeLogControllerDecorator } from "../../../../../main/factories/decorators/log-controller.decorator-factory"
+import { makeDbLoadSurveys } from "../../../../../main/factories/usecases/survey/load-survey/db-load-survey-factory"
+import { LoadSurveyController } from "../../../../../presentation/controllers/survey/load-survey/load-survey-controller"
+import { Controller } from "../../../../../presentation/protocols"
+
+
+export const makeLoadSurveyController = (): Controller => {
+  const controller = new LoadSurveyController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(controller)
+}
---
+++ src/main/factories/usecases/survey/load-survey/db-load-survey-factory.ts
@@ -0,0 +1,9 @@
+import { DbLoadSurveys } from "../../../../../data/usecases/load-surveys/db-load-surveys";
+import { LoadSurvey } from "../../../../../domain/usecases/load-survey";
+import { SurveyMongoRepository } from "../../../../../infra/db/mongodb/survey/survey-mongo-repository";
+
+
+export const makeDbLoadSurveys = (): LoadSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbLoadSurveys(surveyMongoRepository)
+}
