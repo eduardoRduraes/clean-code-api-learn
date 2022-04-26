c8745bf1fd9f1b83c043312a67c1686c8a24eb03
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-04-25 22:45:59 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurveysUsecase throws

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurvey, ok } from "./load-survey-controller-protocols";
+import { Controller, HttpRequest, HttpResponse, LoadSurvey, ok, serverError } from "./load-survey-controller-protocols";
 
 import {} from '../../../../domain/usecases/load-survey'
 export class LoadSurveyController implements Controller {
@@ -6,7 +6,11 @@ export class LoadSurveyController implements Controller {
   constructor(private readonly loadSurvey: LoadSurvey){}
 
   async handle(httpRequest: HttpRequest):Promise<HttpResponse> {
-    const surveys = await this.loadSurvey.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurvey.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
