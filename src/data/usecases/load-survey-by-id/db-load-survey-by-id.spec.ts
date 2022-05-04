d459e16a7eb99b8050a40c2ecf167a36aaa0486a
eduardoDuraes <eduardoduraes.bsi@gmail.com>
2022-05-04 08:02:20 -0300

    test: ensure DbLoadSurveyId returns a Survey if LoadSurveyByIdRepository succeeds

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -55,4 +55,17 @@ describe('DbLoadSurveyById', () => {
     expect(loadByIdSpy).toHaveBeenCalledWith(makeFakeSurveys().id)
   })
 
+  test('Should throw if LoadSurveyByIdRepository throws', () => {
+    const { sut,loadSurveyByIdRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockReturnValueOnce(new Promise((resolve,reject)=>reject(new Error())))
+    const promise =  sut.loadById(makeFakeSurveys().id)
+    expect(promise).rejects.toThrow()
+  })
+
+  test('Should return Survey on success', async () => {
+    const { sut } = makeSut()
+    const survey =  await sut.loadById(makeFakeSurveys().id)
+    expect(survey).toEqual(makeFakeSurveys())
+  })
+
 })
