2b94c9d81b5f13e94a1da0c7800700572d64f093
eduardoRduraes <eduardoduraes.bsi@gmail.com>
2022-05-12 21:35:57 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is now result

--- src/data/usecases/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/data/usecases/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,10 +1,24 @@
+import { LoadSurveyByIdRepository } from "@/data/protocols/db/survey/load-surveys-by-id-repository"
 import { LoadSurveyResult, LoadSurveyResultRepository, SurveyResultModel } from "./load-survey-result-controller-protocols"
 
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor(private readonly loadSurveyResultRepository:LoadSurveyResultRepository){}
-  async load(surveyId: string): Promise<SurveyResultModel> {
-    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+  constructor(
+    private readonly loadSurveyResultRepository:LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository){}
+
+    async load(surveyId: string): Promise<SurveyResultModel> {
+    let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    if(!surveyResult){
+      const surveyModel = await this.loadSurveyByIdRepository.loadById(surveyId)
+      surveyResult = {
+        surveyId: surveyModel.id,
+        question: surveyModel.question,
+        answers: surveyModel.answers.map(answer => Object.assign({}, answer, {count:0, percent:0})),
+        date: surveyModel.date,
+
+      }
+    }
     return surveyResult
   }
 }
--- src/main/factories/usecases/survey/survey-result/save-survey-result/db-save-survey-factory.ts
+++ src/main/factories/usecases/survey/survey-result/save-survey-result/db-save-survey-factory.ts
@@ -1,9 +1,10 @@
 import { DbSaveSurveyResult } from "@/data/usecases/survey-result/save-survey-result/save-survey-result-controller";
 import { SaveSurveyResult } from "@/domain/usecases/save-survey-result";
 import { SurveyResultMongoRepository } from "@/infra/db/mongodb/survey-result/survey-result-mongo-repository";
+import { SurveyMongoRepository } from "@/infra/db/mongodb/survey/survey-mongo-repository";
 
 
 export const makeDbSaveResultSurveys = (): SaveSurveyResult => {
   const surveyResultMongoRepository = new SurveyResultMongoRepository()
-  return new DbSaveSurveyResult(surveyResultMongoRepository,surveyResultMongoRepository)
+  return new DbSaveSurveyResult(surveyResultMongoRepository, surveyResultMongoRepository)
 }
